dd010e5bc796ce71d7a7c8d6af845976d99676a6
Jochen Wezel <jwezel@compumaster.de>
2019-11-05 22:02:39 +0100

    Use dir separator char of current platform environment to support linux

--- XlsEpplus.Test/GlobalTestSetup.vb
+++ XlsEpplus.Test/GlobalTestSetup.vb
@@ -3,7 +3,7 @@
     Public Class GlobalTestSetup
 
         Public Shared Function PathToTestFiles(subPath As String) As String
-            Return System.IO.Path.Combine(System.IO.Path.Combine(System.Reflection.Assembly.GetExecutingAssembly.Location, ".."), subPath)
+            Return System.IO.Path.Combine(System.IO.Path.Combine(System.Reflection.Assembly.GetExecutingAssembly.Location, ".."), subPath.Replace("\", System.IO.Path.DirectorySeparatorChar))
         End Function
 
     End Class
