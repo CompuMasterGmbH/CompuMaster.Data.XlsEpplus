31da05c420a840db4801a79eef7284676745c2a0
Thomas Schädler <tschaedler@compuamster.de>
2021-07-14 13:06:58 +0200

    add Nunit-Test netcoreapp3.1 and net5.0

---
+++ XlsEpplus.Test_new/GlobalTestSetup.vb
@@ -0,0 +1,11 @@
+﻿Namespace CompuMaster.Test.Data
+
+    Public NotInheritable Class GlobalTestSetup
+
+        Public Shared Function PathToTestFiles(subPath As String) As String
+            Return System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly.Location), subPath.Replace("\", System.IO.Path.DirectorySeparatorChar))
+        End Function
+
+    End Class
+
+End Namespace
---
+++ XlsEpplus.Test_new/SupporedExcelFeatures.vb
@@ -0,0 +1,24 @@
+﻿Imports NUnit.Framework
+Imports System.Data
+
+Namespace CompuMaster.Test.Data
+
+    <TestFixture()> Public Class SupportedExcelFeatures
+
+        <Test> Sub TableWithCalculatedCellsTargettingTableColumnsByColumnName()
+            Dim file As String = GlobalTestSetup.PathToTestFiles("testfiles\tablevalues.xlsx")
+            'Dim ds As DataSet = CompuMaster.Data.XlsEpplus.ReadDataSetFromXlsFile(file, True)
+            Dim dt As DataTable
+            dt = CompuMaster.Data.XlsEpplus.ReadDataTableFromXlsFile(file)
+            Dim ColCount As Integer = dt.Columns.Count
+            dt = CompuMaster.Data.DataTables.CopyDataTableWithSubsetOfRows(dt, 5)
+            Dim FormattedTextTable As String = CompuMaster.Data.DataTables.ConvertToPlainTextTableFixedColumnWidths(dt)
+            Console.WriteLine(FormattedTextTable)
+            If FormattedTextTable.Contains("NotImplementedException") Then
+                Assert.Fail("Found cell content with ""NotImplementedException""")
+            End If
+        End Sub
+
+    End Class
+
+End Namespace
